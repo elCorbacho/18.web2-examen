5f9fea10c3a7b3797f1c0a3c89de7da1a4d1989b
ElCorbacho <histopat.cor@gmail.com>
2025-12-20 12:48:00 -0300

    refactor: simplifica las importaciones en AlbumRequestDTO y mejora las validaciones de los campos

--- src/main/java/ipss/web2/examen/dtos/AlbumRequestDTO.java
+++ src/main/java/ipss/web2/examen/dtos/AlbumRequestDTO.java
@@ -1,7 +1,6 @@
 package ipss.web2.examen.dtos;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,11 +13,15 @@ import lombok.NoArgsConstructor;
 public class AlbumRequestDTO {
     
     @NotBlank(message = "El nombre del álbum es obligatorio")
+    @Size(min = 2, max = 100, message = "El nombre debe tener entre 2 y 100 caracteres")
     private String nombre;
     
     @NotNull(message = "El año de lanzamiento es obligatorio")
+    @Min(value = 1900, message = "El año debe ser mayor a 1900")
+    @Max(value = 2100, message = "El año debe ser menor a 2100")
     private Integer year;
     
     @NotBlank(message = "La descripción es obligatoria")
+    @Size(max = 500, message = "La descripción no puede exceder 500 caracteres")
     private String descripcion;
 }
